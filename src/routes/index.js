0c824fe25ea907f253aaab8a338359f481b0ed30
arauujo <wesleey988@gmail.com>
2024-10-13 16:42:38 -0300

    updated sessions routes at index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 const usersRouter = require("./users.routes");
 const movieNotesRouter = require("./movie_notes.routes");
 const movieTagsRouter = require("./movie_tags.routes");
+const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
 routes.use("/movie_notes", movieNotesRouter);
 routes.use("/movie_tags", movieTagsRouter);
+routes.use("/sessions", sessionsRouter);
 
 module.exports = routes;
