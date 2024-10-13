fb6a8f9c0241e55cbad33ea6f782723507d888de
arauujo <wesleey988@gmail.com>
2024-10-13 17:50:53 -0300

    installed and configured cors

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 require("express-async-errors");
 
 const AppError = require("./utils/AppError");
-
+const cors = require('cors');
 const express = require("express");
-
 const routes = require("./routes")
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 app.use(( error, request, response, next) => {
