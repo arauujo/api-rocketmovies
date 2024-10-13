1585d63671267b881496553d50b2f5cbc8c16e43
arauujo <wesleey988@gmail.com>
2024-10-13 20:00:10 -0300

    added middleware to routes and removed parameter user id

--- src/routes/movie_notes.routes.js
+++ src/routes/movie_notes.routes.js
@@ -1,13 +1,15 @@
 const { Router } = require("express")
 
 const MovieNotesController = require("../controllers/MovieNotesController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const movieNotesRoutes = Router();
 
 const movieNotesController = new MovieNotesController();
 
+movieNotesRoutes.use(ensureAuthenticated);
 movieNotesRoutes.get("/", movieNotesController.index);
-movieNotesRoutes.post("/:user_id", movieNotesController.create);
+movieNotesRoutes.post("/", movieNotesController.create);
 movieNotesRoutes.get("/:id", movieNotesController.show);
 movieNotesRoutes.delete("/:id", movieNotesController.delete);
 
--- src/routes/movie_tags.routes.js
+++ src/routes/movie_tags.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express")
 
 const MovieTagsController = require("../controllers/MovieTagsController");
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
 const movieTagsRoutes = Router();
 
 const movieTagsController = new MovieTagsController();
 
-movieTagsRoutes.get("/:user_id", movieTagsController.index);
+movieTagsRoutes.get("/", ensureAuthenticated, movieTagsController.index);
 
 module.exports = movieTagsRoutes;
