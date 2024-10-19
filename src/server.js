6acf17150b48c9aa194c1bd408e7099f4827e674
arauujo <wesleey988@gmail.com>
2024-10-19 18:13:02 -0300

    Serve static files from UPLOADS_FOLDER under /files endpoint

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
 require("express-async-errors");
 
-const migrationsRun = require('./database/sqlite/migrations');
-const AppError = require("./utils/AppError");
-const cors = require('cors');
 const express = require("express");
+const cors = require('cors');
+const migrationsRun = require('./database/sqlite/migrations');
 const routes = require("./routes")
+const AppError = require("./utils/AppError");
+const uploadConfig = require('./configs/upload');
 
 migrationsRun();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
 
 app.use(( error, request, response, next) => {
